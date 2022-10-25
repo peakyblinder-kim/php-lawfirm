89bb05ee6716ee86270f2683b5b2b26ac9c0e45d
peakyblinder-kim <56843503+peakyblinder-kim@users.noreply.github.com>
2022-10-25 12:41:46 +0300

    Add files via upload

---
+++ login.sql
@@ -0,0 +1,12 @@
+CREATE TABLE login(
+    fullname varchar(40) NOT NULL,
+    phone varchar(40) NOT NULL,
+    email varchar(40) NOT NULL,
+    address varchar(40) NOT NULL,
+    education varchar(40) NOT NULL,
+    profile varchar(40) NOT NULL,
+    role varchar(40) NOT NULL,
+    username varchar(40) NOT NULL,
+    password varchar(40) NOT NULL
+
+);
